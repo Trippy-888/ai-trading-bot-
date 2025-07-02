545c8a3f25fe84fee9e037a96c40c4d39187c8c3
Trippy-888 <anand.mandlik78@gmail.com>
2025-07-02 23:04:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ TP_MULTIPLIERS = {
 }
 
 trades_today = 0
-today_date = datetime.utcnow().date()
+today_date = datetime.now(timezone.utc).date() 
 last_trade_time = {}  # Prevent overtrading same pair
 
 # ========== INSTITUTIONAL FUNCTIONS ==========
