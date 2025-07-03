20428e616a136e5875fbf9942b9b4974379fb2d5
Trippy-888 <anand.mandlik78@gmail.com>
2025-07-03 20:03:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,6 @@ def webhook():
     send_telegram_message(message)
     return "Alert processed", 200
 
-if __name__ == '__main__':
-port = int(os.environ.get("PORT", 8080))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
