9c8cadba85754b3ae4c6c9b7b95d9cc75d2e8257
Trippy-888 <anand.mandlik78@gmail.com>
2025-07-10 01:19:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+# main.py
 import os
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Server is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
